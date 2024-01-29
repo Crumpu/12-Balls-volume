828754208446eb10261922f6dc555986f521d2d7
denis <denispolyarush1405@gmail.com>
2024-01-29 22:56:00 +0300

    before layout

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 'use strict';
 let result = 0;
+let list = document.getElementById('list');
+let counter = 0;
 for (let r = 240; r <= 350; r++) {
   if (r % 10 !== 0) continue;
   let V = (3 / 4) * Math.PI * r ** 3;
-    result += V;
+  let formula = `(3 / 4) * Pi * (${r}) ^ 3`;
+  result += V;
+  counter++;
+  list.innerHTML +=
+    '<li>' + formula + ' = ' + ((V / 10 ** 9).toFixed(3))+ 'm3' + '</li>';
 }
 
 let resultInSI = result / 10 ** 9;
-  document.getElementById('testData').textContent = resultInSI.toFixed(3);
+document.getElementById('testData').textContent = resultInSI.toFixed(3);
