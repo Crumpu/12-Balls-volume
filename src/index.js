6a7baa4dc8718e75d245bd6e71d1290be1c591e3
denis <denispolyarush1405@gmail.com>
2024-01-31 03:20:01 +0300

    Finished

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 'use strict';
 let result = 0;
 let list = document.getElementById('list');
-let counter = 0;
 for (let r = 240; r <= 350; r++) {
   if (r % 10 !== 0) continue;
   let V = (3 / 4) * Math.PI * r ** 3;
   let formula = `(3 / 4) * Pi * (${r}) ^ 3`;
   result += V;
-  counter++;
   list.innerHTML +=
     '<li>' + formula + ' = ' + ((V / 10 ** 9).toFixed(3))+ 'm3' + '</li>';
 }
