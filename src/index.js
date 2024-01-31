c751fcdb5f66dec6a5f996ba3f352e186b00676f
denis <denispolyarush1405@gmail.com>
2024-01-31 15:26:26 +0300

    formula corrected

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ let result = 0;
 let list = document.getElementById('list');
 for (let r = 240; r <= 350; r++) {
   if (r % 10 !== 0) continue;
-  let V = (3 / 4) * Math.PI * r ** 3;
-  let formula = `(3 / 4) * Pi * (${r}) ^ 3`;
+  let V = (4 / 3) * Math.PI * r ** 3;
+  let formula = `(4 / 3) * Pi * (${r}) ^ 3`;
   result += V;
   list.innerHTML +=
     '<li>' + formula + ' = ' + ((V / 10 ** 9).toFixed(3))+ 'm3' + '</li>';
