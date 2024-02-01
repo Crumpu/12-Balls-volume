2a90f2c66429fd476ce5177c87b6bc5eb455b17c
denis <denispolyarush1405@gmail.com>
2024-02-01 14:17:30 +0300

    Bugs fixed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 'use strict';
 let result = 0;
-let list = document.getElementById('list');
-for (let r = 240; r <= 350; r++) {
-  if (r % 10 !== 0) continue;
+let wallThickness = 10;
+let minRadius = 240;
+let quantityOfBalls = 12;
+for (let i = 0; i < quantityOfBalls; i++) {
+  let r = minRadius + wallThickness * i;
   let V = (4 / 3) * Math.PI * r ** 3;
-  let formula = `(4 / 3) * Pi * (${r}) ^ 3`;
   result += V;
+  let formula = `(4 / 3) * Pi * (${r}) ^ 3`;
   list.innerHTML +=
-    '<li>' + formula + ' = ' + ((V / 10 ** 9).toFixed(3))+ 'm3' + '</li>';
+    '<li>' + formula + ' = ' + (V / 10 ** 9).toFixed(3) + 'm3' + '</li>';
 }
 
 let resultInSI = result / 10 ** 9;
